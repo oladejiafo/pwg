f35cb5b4f421dada1896da372b758c7cfc7acb02
Shamsi253 <shamsi253@gmail.com>
2022-09-02 14:27:52 +0400

    update password

--- app/Actions/Fortify/UpdateUserPassword.php
+++ app/Actions/Fortify/UpdateUserPassword.php
@@ -4,7 +4,12 @@ namespace App\Actions\Fortify;
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
 use Laravel\Fortify\Contracts\UpdatesUserPasswords;
+use Illuminate\Support\Facades\Auth;
+use App\Mail\ResetPassword;
+use Carbon\Carbon; 
+use DB;
 
 class UpdateUserPassword implements UpdatesUserPasswords
 {
@@ -19,17 +24,41 @@ class UpdateUserPassword implements UpdatesUserPasswords
      */
     public function update($user, array $input)
     {
-        Validator::make($input, [
-            'current_password' => ['required', 'string'],
-            'password' => $this->passwordRules(),
-        ])->after(function ($validator) use ($user, $input) {
-            if (! isset($input['current_password']) || ! Hash::check($input['current_password'], $user->password)) {
-                $validator->errors()->add('current_password', __('The provided password does not match your current password.'));
-            }
-        })->validateWithBag('updatePassword');
+        if(!isset($input['otp'])){
+            Validator::make($input, [
+                'current_password' => ['required', 'string'],
+                'password' => $this->passwordRules(),
+            ])->after(function ($validator) use ($user, $input) {
+                if (! isset($input['current_password']) || ! Hash::check($input['current_password'], $user->password)) {
+                    $validator->errors()->add('current_password', __('The provided password does not match your current password.'));
+                }
+            })->validateWithBag('updatePassword');
 
-        $user->forceFill([
-            'password' => Hash::make($input['password']),
-        ])->save();
+            $token = rand(100000,999999);
+
+            DB::table('password_resets')->insert(
+                ['email' => Auth::user()->email, 'token' => $token, 'created_at' => Carbon::now()]
+            );
+            Mail::to(Auth::user()->email)->send(new ResetPassword($token));
+
+        } else {
+            $tokenData =  DB::table('password_resets')->where('email', Auth::user()->email)->latest('created_at')->first();
+            Validator::make($input, [
+                'current_password' => ['required', 'string'],
+                'password' => $this->passwordRules(),
+                'otp' => 'required'
+            ])->after(function ($validator) use ($user, $input, $tokenData) {
+                if (! isset($input['current_password']) || ! Hash::check($input['current_password'], $user->password)) {
+                    $validator->errors()->add('current_password', __('The provided password does not match your current password.'));
+                }
+                if($tokenData->token != $input['otp']){
+                    $validator->errors()->add('otp', __('Please check the otp in your mail'));
+                }
+            })->validateWithBag('updatePassword');
+
+            $user->forceFill([
+                'password' => Hash::make($input['password']),
+            ])->save();
+        }
     }
 }
--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -8,30 +8,37 @@
     </div>
         <div class="col-span-12 sm:col-span-4" style="width:70%; margin: 0 auto;margin-bottom:20px">
             <x-jet-label for="current_password" value="{{ __('Current Password') }}" />
-            <x-jet-input id="current_password" type="password" class="mt-1 block w-full" wire:model.defer="state.current_password" autocomplete="current-password" />
+            <x-jet-input id="current_password" type="password" class="mt-1 block w-full" wire:model.defer="state.current_password" autocomplete="current-password"/>
             <x-jet-input-error for="current_password" class="mt-2" />
         </div>
 
         <div class="col-span-12 sm:col-span-4" style="width:70%; margin: 0 auto;margin-bottom:20px">
             <x-jet-label for="password" value="{{ __('New Password') }}" />
-            <x-jet-input id="password" type="password" class="mt-1 block w-full" wire:model.defer="state.password" autocomplete="new-password" />
+            <x-jet-input id="password" type="password" class="mt-1 block w-full" wire:model.defer="state.password" autocomplete="new-password"/>
             <x-jet-input-error for="password" class="mt-2" />
         </div>
 
         <div class="col-span-12 sm:col-span-4" style="width:70%; margin: 0 auto;margin-bottom:20px">
             <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-            <x-jet-input id="password_confirmation" type="password" class="mt-1 block w-full" wire:model.defer="state.password_confirmation" autocomplete="new-password" />
+            <x-jet-input id="password_confirmation" type="password" class="mt-1 block w-full" wire:model.defer="state.password_confirmation" autocomplete="new-password"/>
             <x-jet-input-error for="password_confirmation" class="mt-2" />
+        </div>
+
+        <div class="col-span-12 sm:col-span-4 otp-profile" style="width:70%; margin: 0 auto;margin-bottom:20px;">
+            <x-jet-label for="otp" value="{{ __('OTP') }}" />
+            <x-jet-input id="otp" type="password" class="mt-1 block w-full" wire:model.defer="state.otp" autocomplete="otp" />
+            <x-jet-input-error for="otp" class="mt-2" />
+            <i>{{ __('Please check your mail for otp.') }} </i>
         </div>
     </x-slot>
 
     <x-slot name="actions">
-        <x-jet-action-message class="mr-3" on="saved">
+        {{-- <x-jet-action-message class="mr-3" on="saved">
             {{ __('Saved.') }}
-        </x-jet-action-message>
+        </x-jet-action-message> --}}
 
-        <x-jet-button>
-            {{ __('Save') }}
+        <x-jet-button class="updatePassword">
+            {{ __('Update') }}
         </x-jet-button>
     </x-slot>
 </x-jet-form-section>
