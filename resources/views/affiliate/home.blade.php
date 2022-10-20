308afde3248c1f9c7105a21ace7afd45ee0209c0
Shamsi253 <shamsi253@gmail.com>
2022-10-20 17:20:56 +0400

    landing page responsive

--- resources/views/affiliate/home.blade.php
+++ resources/views/affiliate/home.blade.php
@@ -37,32 +37,32 @@
         <div class="row justify-content-md-center">
             <div class="col-12">
                 <div class="row">
-                    <div class="col-4">
+                    <div class="col-sm-12 col-md-12 col-lg-4">
                         <div class="step-1">
                             <img src="{{asset('images/affiliate/Step1.svg')}}" width="100%" height="100%" >
                             <div class="step-1-desc">
                                 <h4>Register on our platform</h4>
-                                <p>Easy registration,few details and you </br> are set to go </p>
+                                <p>Easy registration,few details and you are set to go </p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-4">
+                    <div class="col-sm-12 col-md-12 col-lg-4">
                         <div class="step-2">
                             <img src="{{asset('images/affiliate/Step2.svg')}}" width="100%" height="100%">
                             <div class="step-2-desc">
                                 <h5>Refer your friend & Earn</h5>
-                                <p>Invite friends and other people to<br>
-                                    have their Europe dream come true <br>
+                                <p>Invite friends and other people to
+                                    have their Europe dream come true 
                                    by working in Poland </p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-4">
+                    <div class="col-sm-12 col-md-12 col-lg-4">
                         <div class="step-3">
                             <img src="{{asset('images/affiliate/Step3.svg')}}" width="100%" height="100%">
                             <div class="step-3-desc">
                                 <h5>Withdraw your Earnings</h5>
-                                <p>Transfer your earnings to your prefered <br>
+                                <p>Transfer your earnings to your prefered 
                                     bank account.</p>
                             </div>
                         </div>
@@ -83,51 +83,55 @@
                         Learn how to start earning with PWG Group Refferal program.
                     </p>
                 </div>
-                <div class="row faq-accordion">
-                    <div class="col-6">
-                        <div class="accordion accordion-flush" id="accordionFlushFaqLeft">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="flush-headingThree">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                        How to become a PWG Group Direct affiliate?
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushFaqLeft">
-                                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                <div class="faq-accordion">
+                    <div class="accordion accordion-flush" id="accordionFlushFaqLeft">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-12 col-lg-6">
+                                <div class="row accordion-left">                        
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="flush-headingThree">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                                What is PWG Group?
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushFaqLeft">
+                                            <div class="accordion-body">PWG Group is an immigration company that helps students and professionals migrate abroad to either pursue their studies or careers.</div>
+                                        </div>
+                                    </div>
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="faq-four">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-faq" aria-expanded="false" aria-controls="flush-faq">
+                                                Is PWG legitimate?
+                                            </button>
+                                        </h2>
+                                        <div id="flush-faq" class="accordion-collapse collapse" aria-labelledby="faq-four" data-bs-parent="#accordionFlushFaqLeft">
+                                            <div class="accordion-body">Yes,  PWG Group is registered in the UAE and has been in operation for 8 years. It has helped more than 3000 professionals to migrate to Poland, Germany, Czech Republic, Malta and Canada. You can check out some testimonial videos from some of our successful applicants here.</div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="faq-four">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-faq" aria-expanded="false" aria-controls="flush-faq">
-                                        How to become a PWG Group Direct affiliate?
-                                    </button>
-                                </h2>
-                                <div id="flush-faq" class="accordion-collapse collapse" aria-labelledby="faq-four" data-bs-parent="#accordionFlushFaqLeft">
-                                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="accordion accordion-flush" id="accordionFlushFaqRight">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="flush-headingOne">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                        How to become a PWG Group Direct affiliate?
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushFaqRight">
-                                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
-                                </div>
-                            </div>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="flush-headingTwo">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                        How to become a PWG Group Direct affiliate?
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushFaqRight">
-                                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                            <div class="col-sm-12 col-md-12 col-lg-6">
+                                <div class="row accordion-right">
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="flush-headingOne">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                                How do I pay for my application process?
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushFaqLeft">
+                                            <div class="accordion-body">You must pay a down payment to start the application process, followed by a second installment once the visa is approved. The third and final payment is made after you arrive at your dream destination. </div>
+                                        </div>
+                                    </div>
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="flush-headingTwo">
+                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                                I’m outside the UAE, can I apply for my visa through PWG?
+                                            </button>
+                                        </h2>
+                                        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushFaqLeft">
+                                            <div class="accordion-body">Yes, you can apply virtually from any country by contacting us via Whatsapp +971 50 423 0438</div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
