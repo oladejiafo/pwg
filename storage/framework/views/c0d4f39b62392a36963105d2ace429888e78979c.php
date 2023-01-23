38fa0c005477d73078ca6769321486bcfe4458bb
oladejiafo <dejigegs@gmail.com>
2023-01-23 15:08:46 +0400

    updates

--- storage/framework/views/c0d4f39b62392a36963105d2ace429888e78979c.php
+++ storage/framework/views/c0d4f39b62392a36963105d2ace429888e78979c.php
@@ -1,15 +1,26 @@
 <style>
+    .footer {
+        font-size:15px;
+     }
     .footer a {
         font-size:15px;
         margin: 0px;
     }
     .footer a:hover {
         color:blue;
-        font-size:15px;
     }
 
     hr {
         border: 2px solid #fff;
+    }
+    @media (max-width:360px){
+     .footer {
+        font-size:12px;
+     }
+     .footer a {
+        font-size:12px;
+        margin: 0px;
+     }
     }
 </style>
 <div class="footer" align="right" style="padding:20px; ">
