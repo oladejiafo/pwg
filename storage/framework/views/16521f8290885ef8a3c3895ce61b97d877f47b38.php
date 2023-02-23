b22d50741000ea4a10632240f3452ac7d02620a0
oladejiafo <dejigegs@gmail.com>
2023-02-23 15:14:10 +0400

    more payment options

--- storage/framework/views/16521f8290885ef8a3c3895ce61b97d877f47b38.php
+++ storage/framework/views/16521f8290885ef8a3c3895ce61b97d877f47b38.php
@@ -298,6 +298,23 @@
                 <?php endif; ?>
             <?php endif; ?>
         </div>
+
+        
+        <div class="cols col-span-12 sm:col-span-12" style="width:70%; margin: 25px auto">
+            <label for="Agent_Name">Agent Name</label>
+            <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'jetstream::components.input','data' => ['id' => 'Agent_Name','value' => ''.e(Auth::user()->sales_agent_name_by_client).'','type' => 'text','class' => 'mt-1 block w-full','ariaReadonly' => '','readonly' => true]]); ?>
+<?php $component->withName('jet-input'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes(['id' => 'Agent_Name','value' => ''.e(Auth::user()->sales_agent_name_by_client).'','type' => 'text','class' => 'mt-1 block w-full','aria-readonly' => '','readonly' => true]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+        </div>
  
          <?php $__env->slot('actions', null, []); ?> 
         <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
